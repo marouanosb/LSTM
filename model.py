99252edb9ed180458bc28d29581114c1ede75548
unknown <marouanosb@gmail.com>
2024-11-22 20:31:07 +0100

    dataset

--- model.py
+++ model.py
@@ -1,13 +1,24 @@
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 
-def Initialisation(X):
+# %%
+X, y = make_blobs(n_samples=100, n_features=2, centers=2, random_state=0)
+y = y.reshape((y.shape[0], 1))
 
-def Model(X, W, b):
+plt.scatter(X[:, 0],X[:,1], c=y)
+plt.show()
 
-def Cost(A, y)
+# %%
+#def Initialisation(X):
+
+#def Model(X, W, b):
+
+#def Cost(A, y)
     
-def Gradients(A, X, y)
+#def Gradients(A, X, y)
     
-Update(W, b, dW, db)
+#def Update(W, b, dW, db)
+
+# %%
