1d6c5414d1d8500179bef255c8ce92ef85bc93bf
unknown <marouanosb@gmail.com>
2024-11-22 18:50:17 +0100

    initial commit

---
+++ model.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+
+def Initialisation(X):
+
+def Model(X, W, b):
+
+def Cost(A, y)
+    
+def Gradients(A, X, y)
+    
+Update(W, b, dW, db)
