d1188cea118ca307274904ffab7dc8690917b74b
unknown <marouanosb@gmail.com>
2024-12-01 16:32:34 +0100

    added LSTM layers and prediction

--- lstm.py
+++ lstm.py
@@ -26,10 +26,10 @@ tf.random.set_seed(7)
 scaler = MinMaxScaler(feature_range=(0,1))
 dataset = scaler.fit_transform(dataset)
 # split dataset into train and test set
-trainsize = int(len(dataset)*0.8)   # 80% of dataset for training
-testsize = len(dataset) - trainsize # the rest (20%) for testing
-trainset = dataset[0:trainsize]
-testset = dataset[trainsize:]
+trainSize = int(len(dataset)*0.8)   # 80% of dataset for training
+testSize = len(dataset) - trainSize # the rest (20%) for testing
+trainset = dataset[0:trainSize]
+testset = dataset[trainSize:]
 # split the dataset into X and Y arrays where X are t and Y t+1
 def split_dataset(dataset, ):
     dataX, dataY = [], []
@@ -43,4 +43,51 @@ testX, testY = split_dataset(testset)
 #reshape inputs as [samples, timesteps, features]
 trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+# %%
+# implementation
+
+# create lstm network 
+model = Sequential()
+model.add(tf.keras.Input((1,1)))    # input layer
+model.add(LSTM(4))  # hidden layer
+model.add(Dense(1)) # output layer 
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=1)
+
+# %%
+# predictions
+
+# predict
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+# inverse normalisation
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform(trainY)
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform(testY)
+# calculate error
+trainScore = np.sqrt(mean_squared_error(trainY, trainPredict))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = np.sqrt(mean_squared_error(testY, testPredict))
+print('Test Score: %.2f RMSE' % (testScore))
+
+# %%
+# plotting
+
+
+# shift train predictions for plotting
+trainPredictPlot = np.empty_like(dataset)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[1:len(trainPredict)+1, :] = trainPredict
+# shift test predictions for plotting
+testPredictPlot = np.empty_like(dataset)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(trainPredict)+1:len(dataset)-1, :] = testPredict
+
+plt.plot(scaler.inverse_transform(dataset), c='green')
+plt.plot(trainPredictPlot, c='blue')
+plt.plot(testPredictPlot, c='red')
+plt.legend(['dataset', 'training', 'prediction'])
+plt.show()
 # %%
